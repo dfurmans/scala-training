21d02f2ebc428a8d8711d64cc830f37fbfbf4891
Dawid Furman <dfurmans@gmail.com>
2021-01-01 15:16:30 +0100

    extension0 :: a Type Class example for the Math

--- src/main/scala/info/maalvarez/lesson01/Math.scala
+++ src/main/scala/info/maalvarez/lesson01/Math.scala
@@ -5,3 +5,14 @@ trait Math[A] {
   def sum(x: Tuple[A]): A
   def operate(operation: (A, A) => A, x: A, y: A): A
 }
+
+// As a ad-hoc Polymorphism
+object Math{
+  implicit object MathLikeForInt extends Math[Int] {
+    def sum(x: Int, y: Int): Int = x + y
+    def sum(x: Tuple[Int]): Int = sum(x.first, x.second)
+    def operate(operation: (Int, Int) => Int, x: Int, y: Int): Int = operation(x, y)
+  }
+  // Easy add MathLike behaviour for another type T as an simple
+  // extension Math[Int]
+}
