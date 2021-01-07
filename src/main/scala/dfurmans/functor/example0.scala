72793883638e6ee397be24947d8a3ef19e1c8dca
Dawid Furman <dfurmans@gmail.com>
2021-01-07 17:31:15 +0100

    Example0 - add functor law and make the example0 executable"

--- src/main/scala/dfurmans/functor/example0.scala
+++ src/main/scala/dfurmans/functor/example0.scala
@@ -2,22 +2,9 @@ package dfurmans.functor
 
 import scala.collection.immutable.Queue
 
-object Example0 {
+object Example0 extends App {
 
-  object FunctorDefinition {
-
-    // our functor definition declaration
-    trait Functor[F[_]] {
-      def map[A, B](fa: F[A])(f: A => B): F[B]
-    }
-
-    // a simple functor baked/based on Seq implementation
-    val ourFunctorForSeq: Functor[Seq] = new Functor[Seq] {
-      override def map[A, B](fa: Seq[A])(f: A => B): Seq[B] = fa.map(f)
-    }
-  }
-
-  object Problem0 {
+  object Problem0{
     type HumanName = String
 
     // todo :: change Queue[Human] into Queue[HumanName]
@@ -30,6 +17,34 @@ object Example0 {
     val fromHumanToHumanNameFunction: Human => HumanName = humanName => humanName.name
     val humanName: Seq[HumanName] = FunctorDefinition.ourFunctorForSeq.map(humanQueue)(fromHumanToHumanNameFunction)
 
+  }
+
+  lazy val toPerformerDefinition: String = {
+    s"""
+       |Functor Example ::
+       | => FunctorRules.identityRule -> ${FunctorDefinition.FunctorRules.identityRule}
+       |
+       |""".stripMargin.stripLineEnd
+  }
+
+  println(toPerformerDefinition)
+
+  object FunctorDefinition {
+
+    // our functor definition declaration
+    trait Functor[F[_]] {
+      def map[A, B](fa: F[A])(f: A => B): F[B]
+    }
+
+    // todo :: explain it
+    object FunctorRules{
+      val identityRule = ourFunctorForSeq.map(Seq.empty[Int])(x => x) == Seq.empty[Int]
+    }
+
+    // a simple functor baked/based on Seq implementation
+    val ourFunctorForSeq: Functor[Seq] = new Functor[Seq] {
+      override def map[A, B](fa: Seq[A])(f: A => B): Seq[B] = fa.map(f)
+    }
   }
 
 }
