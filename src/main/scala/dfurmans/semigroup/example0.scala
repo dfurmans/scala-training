62fc81cfc0930326ee53f12669ea24aaf83c95aa
Dawid Furman <dfurmans@gmail.com>
2021-01-08 01:12:54 +0100

    Add Semigroup example

--- src/main/scala/dfurmans/semigroup/example0.scala
+++ src/main/scala/dfurmans/semigroup/example0.scala
@@ -1,7 +1,9 @@
-package dfurmans.functor
+package dfurmans.semigroup
 
 import cats.Semigroup
-object example1 extends App{
+
+object example0 extends App {
+
   import CombineItWithSemigroup.Usage._
 
   val toPerformerDefinition = {
@@ -13,20 +15,27 @@ object example1 extends App{
   }
   print(toPerformerDefinition)
 
-  object CombineItWithSemigroup{
+  object CombineItWithSemigroup {
+
     case class Profit(aProfitValue: Int)
-    implicit  val aSemigroupsForProfit: Semigroup[Profit] = new Semigroup[Profit]{
+
+    implicit val aSemigroupsForProfit: Semigroup[Profit] = new Semigroup[Profit] {
+      // the semigroup type class does not provide an empty element (we need more power from the Monoid)
       override def combine(x: Profit, y: Profit): Profit = Profit(x.aProfitValue + y.aProfitValue)
     }
-    object Usage{
+
+    object Usage {
       val p1 = Profit(1)
       val p2 = Profit(1)
       val combineWithDef: Profit = aSemigroupsForProfit.combine(p1, p2)
       // OR
+
       import cats.implicits._
+
       val combineWithOperator: Profit = p1 |+| p2
 
     }
+
   }
 
 }
